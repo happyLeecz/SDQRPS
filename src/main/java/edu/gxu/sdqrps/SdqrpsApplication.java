db1cf39b7e180c47047c13b6703bc7f08d8a0212
czll4r <1154106524@qq.com>
2019-11-22 19:30:49 +0800

    项目初次提交

---
+++ src/main/java/edu/gxu/sdqrps/SdqrpsApplication.java
@@ -0,0 +1,16 @@
+package edu.gxu.sdqrps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@SpringBootApplication
+public class SdqrpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdqrpsApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/edu/gxu/sdqrps/SdqrpsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.gxu.sdqrps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdqrpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
